3217feb1ecd5ac81d4b9b7194bcc108c136235f3
Natalia59 <maxi77@bk.ru>
2022-10-01 23:27:55 +0200

    Добавили тест 1

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Введите первое число: ");
+string userInput1 = Console.ReadLine() ?? "";
+int userNumber1 = int.Parse(userInput1);
+
+Console.Write("Введите второе число: ");
+string userInput2 = Console.ReadLine() ?? "";
+int userNumber2 = int.Parse(userInput2);
+
+Console.Write("a= " + userNumber1 + ", " + "b= " + userNumber2 + " -> ");
+
+if (userNumber2 > userNumber1);
+{
+    Console.WriteLine ("userNumber1 == Минимум");
+    Console.WriteLine ("userNumber2 == Максимум");
+}
