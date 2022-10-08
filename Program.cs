1705a4e2fcc0d51a5c2c29bef466c641a36f8b06
Natalia59 <maxi77@bk.ru>
2022-10-08 22:32:55 +0200

    Домашнее задание

--- Program.cs
+++ Program.cs
@@ -1,11 +1,30 @@
-﻿// Программа выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+﻿//Напишите программу, которая принимает на вход координаты двух точек и 
+//находит расстояние между ними в 3D пространстве.
+//A (3,6,8); B (2,1,-7), -> 15.84
+//A (7,-5, 0); B (1,-1,9) -> 11.53
 
-int num = new Random().Next(100, 1000);
-Console.WriteLine(num);
-int a1 = num % 10;
-Console.WriteLine(a1);
+
+int x1 = Coordinate("x", "A");
+int y1 = Coordinate("y", "A");
+int z1 = Coordinate("z", "A");
+int x2 = Coordinate("x", "B");
+int y2 = Coordinate("y", "B");
+int z2 = Coordinate("z", "B");
+
+int Coordinate(string coorName, string pointName)
 {
-    if (int.TryParse(Console.ReadLine(), out num))
-    if (99 < num && num < 1000);
+    Console.Write($"Введите координату {coorName} точки {pointName}: ");
+    return Convert.ToInt16(Console.ReadLine());
 }
-    Console.WriteLine($"Третьей цифры нет");
+
+double Decision(double x1, double x2, 
+                double y1, double y2, 
+                double z1, double z2){
+  return Math.Sqrt(Math.Pow((x2-x1), 2) + 
+                   Math.Pow((y2-y1), 2) + 
+                   Math.Pow((z2-z1), 2));
+}
+
+double segmentLength =  Math.Round (Decision(x1, x2, y1, y2, z1, z2), 2 );
+
+Console.WriteLine($"Длина отрезка  {segmentLength}");
